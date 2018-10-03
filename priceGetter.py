ef3058cbe63f2824c60b9135884a806b7a94142e
Emanuel Hernandez <emanuelh77@hotmail.com>
2018-10-02 18:34:49 -0600

    learning beautifulSoup4

---
+++ priceGetter.py
@@ -0,0 +1,17 @@
+import bs4
+import requests
+
+def getAmazonPrice(productURL):
+    res = requests.get(productURL)
+    res.raise_for_status()
+
+    soup = bs4.BeautifulSoup(res.text, 'html.parser')
+    elems = soup.select('#priceblock_ourprice')
+    return elems[0].text.strip()
+
+
+price = getAmazonPrice('https://www.amazon.com/ROLAND-Electronic-Drum-Set-TD-11KV/dp/B00AKQVUSA/ref=sr_1_10?ie=UTF8&qid=1538523379&sr=8-10&keywords=drum+set')
+print('The price is ' + price)
+
+
+
