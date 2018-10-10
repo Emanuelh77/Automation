68269574e93871db1cb18b36b1dd4595bdf944eb
E-man <emanuelh77@hotmail.com>
2018-10-10 01:20:21 -0600

    added some comments

--- priceGetter.py
+++ priceGetter.py
@@ -5,8 +5,9 @@ def getAmazonPrice(productURL):
     res = requests.get(productURL)
     res.raise_for_status()
 
-    soup = bs4.BeautifulSoup(res.text, 'html.parser')
-    elems = soup.select('#priceblock_ourprice')
+    
+    soup = bs4.BeautifulSoup(res.text, 'html.parser')   #soup object
+    elems = soup.select('#priceblock_ourprice')         #elems becomes a list
     return elems[0].text.strip()
 
 
